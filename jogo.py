68fdf152af952dd8acec70013852a830d06f1395
BrunadeJesusNunes <150243685+BrunadeJesusNunes@users.noreply.github.com>
2025-02-21 22:13:49 -0300

    jogo

---
+++ jogo.py
@@ -0,0 +1,16 @@
+print("**********")
+print("Bem vindo ao jogo de Adivinhação")
+peint("**********")
+
+numero_secreto = 96
+
+chute_str = input("Digite o seu número")
+print("Você digetou;" chut-str)
+hute = int (chute_str)
+
+if (numero_secreto == chute):
+    print("Você acertou!")
+
+    alse:
+    print("Você errou!")
+print ("Fim do jogo")
